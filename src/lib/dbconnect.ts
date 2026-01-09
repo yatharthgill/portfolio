8d16f329cbeb1db58d57caddb8df02fb28641a99
yatharthgill <yatharthchaudhary05@gmail.com>
2026-01-10 00:36:27 +0530

    refactor: simplify dbConnect function and remove unused Mongoose cache logic

--- src/lib/dbconnect.ts
+++ src/lib/dbconnect.ts
@@ -1,40 +1,28 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
 
-if (typeof MONGO_URI !== "string") {
-  throw new Error("MONGO_URI is not defined");
+type ConnectObject = {
+    isConnected?: number
 }
 
-type MongooseCache = {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-};
+ 
+const connection: ConnectObject = {}
 
-declare global {
-  // eslint-disable-next-line no-var
-  var _mongoose: MongooseCache | undefined;
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already Connected")
+        return;
+    }
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI || "", {})
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("Db Connected Successfully")
+    } catch (error) {
+        console.log("Db Connection Failed", error)
+        process.exit(1)
+    }
+
 }
 
-let cached = global._mongoose;
-
-if (!cached) {
-  cached = global._mongoose = { conn: null, promise: null };
-}
-
-export default async function dbConnect(): Promise<typeof mongoose> {
-  if (cached!.conn) {
-    return cached!.conn;
-  }
-
-  if (!cached!.promise) {
-    cached!.promise = mongoose
-      .connect(MONGO_URI as string, {
-        bufferCommands: false,
-      })
-      .then((m) => m);
-  }
-
-  cached!.conn = await cached!.promise;
-  return cached!.conn;
-}
+export default dbConnect;
